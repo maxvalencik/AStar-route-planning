29bf4dae5957dc9448f03f578b18e834df72af2e
Maxime VALENCIK <89088940+maxvalencik@users.noreply.github.com>
2021-09-22 11:19:37 -0400

    Add files via upload

--- route_planner.cpp
+++ route_planner.cpp
@@ -11,15 +11,15 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
-    *start_node = m_Model.FindClosestNode(start_x, start_y);
-    *end_node = m_Model.FindClosestNode(end_x, end_y);
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
     //You can use the distance to the end_node for the h value.
-    return node->distance(*end_node); // -> access distance method for pointer node (usually . when not a pointer)
+    float h = node->distance(*end_node); //-> access distance method for pointer node (usually . when not a pointer)
+    return h;
 }
 
 
